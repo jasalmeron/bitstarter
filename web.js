ad892117f22c8a5851f8bb8a8c2fd8363e59277b
jasalmeron <jasalmeron@gmail.com>
2013-07-10 21:52:33 +0000

    fixed ReferenceError: fs is not defined

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buf = fs.readFileSync("html/index.html");
   var msg = buf.toString();
   response.send(msg);
